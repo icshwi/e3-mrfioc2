eff3bc0c5df2283c8ced2c665e54eeff16e3b713
Jeong Han Lee <jeonghan.lee@gmail.com>
2017-11-10 16:27:43 +0100

    update cmd file for 1Hz is generating now

--- cmds/cpci230evg.cmd
+++ cmds/cpci230evg.cmd
@@ -1,5 +1,5 @@
 require devlib2, 2.9.0
-require mrfioc2, 2.2.0
+require mrfioc2, 0.0.0
 require iocStats, 1856ef5
 require autosave, 5.8.0
 
@@ -12,11 +12,19 @@ epicsEnvSet("AUTOSAVE", "/home/timinguser/autosave")
 
 epicsEnvSet("IOC", "FREIA-CPCI:EVG0")
 
-mrmEvgSetupPCI("EVG0", "10:e.0")
+epicsEnvSet("DEVICE" "EVG0")
 
-#dbLoadRecords("cpci-evg-300.db", "SYS=TST, D=evg:1, EVG=EVG1")
+mrmEvgSetupPCI("DEVICE", "10:e.0")
+
+dbLoadRecords("cpci-evg230-ess.db", "SYS=TST, D=evg0, DEVICE=DEVICE")
 
 iocInit()
 
 dbl > "${IOC}_PVs.list"
 
+
+dbpf("TST-evg0:Mxc0-Prescaler-SP" "12491350")
+## Frequency should be 1Hz
+dbpr("TST-evg0:Mxc0-Frequency-RB",1)
+dbpf("TST-evg0:TrigEvt1-EvtCode-SP","122")
+dbpf("TST-evg0:TrigEvt1-TrigSrc-Sel","Mxc0")
