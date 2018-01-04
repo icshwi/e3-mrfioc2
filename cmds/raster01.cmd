0cc6036f3610e04ed256d7eae81d178696c3de72
Jeong Han Lee <jeonghan.lee@gmail.com>
2018-01-04 17:07:56 +0100

    Modified raster01.cmd for standalone operation and created populate_sequencer.cmd

--- cmds/raster01.cmd
+++ cmds/raster01.cmd
@@ -12,25 +12,45 @@ epicsEnvSet("LOCATION","Table at ICS Tuna Lab")
 epicsEnvSet("EPICS_CA_MAX_ARRAY_BYTES","10000000")
 
 epicsEnvSet("IOC", "RM01-PCIE")
+epicsEnvSet("DEV1", "EVR1")
 
 epicsEnvSet("MainEvtCODE" "14")
 epicsEnvSet("HeartBeatEvtCODE"   "122")
 epicsEnvSet("ESSEvtClockRate"  "88.0525")
 
-mrmEvrSetupPCI("EVR1",  "01:00.0")
-#dbLoadRecords("evr-pcie-300dc-ess.db","SYS=$(IOC), D=EVR1")
+mrmEvrSetupPCI("$(DEV1)",  "01:00.0")
+dbLoadRecords("evr-pcie-300dc-ess.db","EVR=$(DEV1), SYS=$(IOC), D=$(DEV1), FEVT=$(ESSEvtClockRate)")
 
 # needed with software timestamp source w/o RT thread scheduling
 var evrMrmTimeNSOverflowThreshold 100000
 
 
 # iocStats
-dbLoadRecords("iocAdminSoft.db", "IOC=$(IOC):IocStats")
+dbLoadRecords("iocAdminSoft.db", "IOC=$(IOC)-IocStats")
 
 
 
 iocInit()
 
-
 dbl > "${IOC}_PVs.list"
 
+dbpf $(IOC)-$(DEV1):Ena-Sel 1
+
+# Get current time from system clock
+dbpf $(IOC)-$(DEV1):TimeSrc-Sel 2
+
+# Set up the prescaler that will trigger the sequencer at 14 Hz
+dbpf $(IOC)-$(DEV1):PS0-Div-SP 6289464
+
+# Set up the sequencer
+dbpf $(IOC)-$(DEV1):SoftSeq0-RunMode-Sel 0 # normal mode
+dbpf $(IOC)-$(DEV1):SoftSeq0-TrigSrc-2-Sel 2 # prescaler 0
+dbpf $(IOC)-$(DEV1):SoftSeq0-TsResolution-Sel 2 # us
+dbpf $(IOC)-$(DEV1):SoftSeq0-Load-Cmd 1
+dbpf $(IOC)-$(DEV1):SoftSeq0-Enable-Cmd 1
+# Remember to run the script that populates the sequencer!
+
+# Set up the output
+dbpf $(IOC)-$(DEV1):DlyGen0-Evt-Trig0-SP $(MainEvtCODE)
+dbpf $(IOC)-$(DEV1):DlyGen0-Width-SP 1000 # Time in us, as selected with $(IOC)-$(DEV1):SoftSeq0-TsResolution-Sel
+dbpf $(IOC)-$(DEV1):OutRB08-Src-SP 0 # Delay generator 0
