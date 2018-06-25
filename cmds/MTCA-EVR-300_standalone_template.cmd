bd61032d29c9ce32b732553956aa44f256a30dfd
Javier Cereijo Garcia <javicereijo@users.noreply.github.com>
2018-06-25 14:49:36 +0200

    Update MTCA-EVR-300_standalone_template.cmd

--- cmds/MTCA-EVR-300_standalone_template.cmd
+++ cmds/MTCA-EVR-300_standalone_template.cmd
@@ -2,11 +2,12 @@
 #
 #require require,2.5.4
 #require devlib2,2.9.0
-require mrfioc2,2.2.0-rc1
+require mrfioc2,2.2.0-rc2
 # require iocStats,1856ef5
 # require autosave, 5.8.0
 
 
+### Define several needed macros ###
 epicsEnvSet("ENGINEER","")
 epicsEnvSet("LOCATION","")
 
@@ -19,10 +20,11 @@ epicsEnvSet("MainEvtCODE" "14")
 epicsEnvSet("HeartBeatEvtCODE"   "122")
 epicsEnvSet("ESSEvtClockRate"  "88.0525")
 
+### Register the EVR with the IOC and load the database ###
 mrmEvrSetupPCI("$(DEV1)",  "06:00.0")
 dbLoadRecords("evr-mtca-300u-ess.db","EVR=$(DEV1), SYS=$(IOC), D=$(DEV1), FEVT=$(ESSEvtClockRate)")
 
-# needed with software timestamp source w/o RT thread scheduling
+### Needed with software timestamp source w/o RT thread scheduling ###
 var evrMrmTimeNSOverflowThreshold 100000
 
 
@@ -36,28 +38,26 @@ iocInit()
 
 # dbl > "${IOC}_PVs.list"
 
-# Get current time from system clock
+### Get current time from system clock ###
 dbpf $(IOC)-$(DEV1):TimeSrc-Sel 2
 
-# Set up the prescaler that will trigger the sequencer at 14 Hz
-dbpf $(IOC)-$(DEV1):PS0-Div-SP 6289424 # in standalone mode because real freq from synthsiezer is
-# 88.05194802 MHz, otherwise 6289464
+### Set up the prescaler that will trigger the sequencer at 14 Hz ###
+dbpf $(IOC)-$(DEV1):PS0-Div-SP 6289424 # in standalone mode because real freq from synthsiezer is 88.05194802 MHz, otherwise 6289464
 
-# Set up the sequencer
-dbpf $(IOC)-$(DEV1):SoftSeq0-RunMode-Sel 0 # normal mode
-dbpf $(IOC)-$(DEV1):SoftSeq0-TrigSrc-2-Sel 2 # prescaler 0
-dbpf $(IOC)-$(DEV1):SoftSeq0-TsResolution-Sel 2 # us
+### Set up the sequencer ###
+dbpf $(IOC)-$(DEV1):SoftSeq0-RunMode-Sel 0 # normal mode, rearm after finish
+dbpf $(IOC)-$(DEV1):SoftSeq0-TrigSrc-2-Sel 2 # trigger the sequence from prescaler 0
+dbpf $(IOC)-$(DEV1):SoftSeq0-TsResolution-Sel 2 # select us as the units of the timestamp array of the seuqncer
 dbpf $(IOC)-$(DEV1):SoftSeq0-Load-Cmd 1
 dbpf $(IOC)-$(DEV1):SoftSeq0-Enable-Cmd 1
 
-# Trigger backplane trigger line X at 14 Hz
+### Trigger backplane trigger line X at 14 Hz ###
 # dbpf $(IOC)-$(DEV1):DlyGen0-Evt-Trig0-SP $(MainEvtCODE)
 # dbpf $(IOC)-$(DEV1):DlyGen0-Width-SP 1000 # time in us, as
 # selected with $(IOC)-$(DEV1):SoftSeq0-TsResolution-Sel
 # dbpf $(IOC)-$(DEV1):OutBackX-Src-SP 0 # trigger from delay generator 0
 
-# Send event clock over backplane clock line TCLKB, MCH should be configured to send the clock back
-# to the AMCs over TCLKA
+### Send event clock over backplane clock line TCLKB, MCH should be configured to send the clock back to the AMCs over TCLKA ###
 # dbpf $(IOC)-$(DEV1):OutTCLKB-Src-SP 63
 # dbpf $(IOC)-$(DEV1):OutTCLKB-Ena-Sel 1
 # dbpf $(IOC)-$(DEV1):OutTCLKB-Pwr-Sel 1
@@ -82,5 +82,6 @@ dbpf $(IOC)-$(DEV1):SoftSeq0-Enable-Cmd 1
 # dbpf $(IOC)-$(DEV1):OutTCLKB-Pat-Low16_31-SP.BD 1
 # dbpf $(IOC)-$(DEV1):OutTCLKB-Pat-Low16_31-SP.BC 1
 
+### Run the script that configures the events and timestamp of the sequence ###
 system("/bin/sh ./configure_sequencer_14Hz.sh $(IOC) $(DEV1)")
 
