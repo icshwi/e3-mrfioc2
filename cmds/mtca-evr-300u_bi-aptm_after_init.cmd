36833fd3b877a163a523da73c1261d78254b7f34
Jeong Han Lee <jeonghan.lee@gmail.com>
2018-08-20 17:43:16 +0200

    tune after init script for MTCA evr for aptm

--- cmds/mtca-evr-300u_bi-aptm_after_init.cmd
+++ cmds/mtca-evr-300u_bi-aptm_after_init.cmd
@@ -16,10 +16,36 @@ dbpf $(IOC)-$(_PORT_EVR):SoftSeq0-TsResolution-Sel 2 # select us as the units of
 dbpf $(IOC)-$(_PORT_EVR):SoftSeq0-Load-Cmd 1
 dbpf $(IOC)-$(_PORT_EVR):SoftSeq0-Enable-Cmd 1
 
-### Trigger backplane trigger line X at 14 Hz ###
-# dbpf $(IOC)-$(_PORT_EVR):DlyGen0-Evt-Trig0-SP $(MainEvtCODE)
-# dbpf $(IOC)-$(_PORT_EVR):DlyGen0-Width-SP 1000 # time in us, as selected with $(IOC)-$(_PORT_EVR):SoftSeq0-TsResolution-Sel
-# dbpf $(IOC)-$(_PORT_EVR):OutBackX-Src-SP 0 # trigger from delay generator 0
+
+### Run the script that configures the events and timestamp of the sequence ###
+system("/bin/sh $(_TOP)/configure_sequencer_14Hz.sh $(IOC) $(_PORT_EVR)")
+
+
+
+################ TRIGGER ALL BACKPLANE LINES ON INPUT ######################
+### Trigger input on signal edge/level ###
+dbpf $(IOC)-$(_PORT_EVR):In0-Trig-Ext-Sel "Edge"
+### Active Edge Rising/Falling ###
+dbpf $(IOC)-$(_PORT_EVR):In0-Edge-Sel "Active Rising"
+### Active Level High/Low ###
+#dbpf $(IOC)-$(_PORT_EVR):In0-Lvl-Sel "Active High"
+### Create event 10 when input is triggered ##
+dbpf $(IOC)-$(_PORT_EVR):In0-Code-Ext-SP 10
+### Trigger delay generator 0 on input ###
+dbpf $(IOC)-$(_PORT_EVR):DlyGen0-Evt-Trig0-SP 10
+### Set width of the trigger in us ###
+dbpf $(IOC)-$(_PORT_EVR):DlyGen0-Width-SP 1000
+### Set delay of the trigger in us ###
+dbpf $(IOC)-$(_PORT_EVR):DlyGen0-Delay-SP 0
+### Triger backplane lines in delay generator 0 ###
+dbpf $(IOC)-$(_PORT_EVR):OutBack0-Src-SP 0 # trigger from delay generator 0
+dbpf $(IOC)-$(_PORT_EVR):OutBack1-Src-SP 0
+dbpf $(IOC)-$(_PORT_EVR):OutBack2-Src-SP 0
+dbpf $(IOC)-$(_PORT_EVR):OutBack3-Src-SP 0
+dbpf $(IOC)-$(_PORT_EVR):OutBack4-Src-SP 0
+dbpf $(IOC)-$(_PORT_EVR):OutBack5-Src-SP 0
+dbpf $(IOC)-$(_PORT_EVR):OutBack6-Src-SP 0
+dbpf $(IOC)-$(_PORT_EVR):OutBack7-Src-SP 0
 
 ### Send event clock over backplane clock line TCLKB, MCH should be configured to send the clock back to the AMCs over TCLKA ###
  dbpf $(IOC)-$(_PORT_EVR):OutTCLKB-Src-SP 63
@@ -45,7 +71,3 @@ dbpf $(IOC)-$(_PORT_EVR):SoftSeq0-Enable-Cmd 1
  dbpf $(IOC)-$(_PORT_EVR):OutTCLKB-Pat-Low16_31-SP.BE 1
  dbpf $(IOC)-$(_PORT_EVR):OutTCLKB-Pat-Low16_31-SP.BD 1
  dbpf $(IOC)-$(_PORT_EVR):OutTCLKB-Pat-Low16_31-SP.BC 1
-
-### Run the script that configures the events and timestamp of the sequence ###
-system("/bin/sh $(_TOP)/configure_sequencer_14Hz.sh $(IOC) $(_PORT_EVR)")
-
