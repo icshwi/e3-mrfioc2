278fcdcb2bb0ab1a3a525f583f22307b16b8041e
Jerzy Jamroz <jerzy.jamroz@esss.se>
2019-03-15 17:00:40 +0100

    ESS naming test.

--- cmds/em/cnt.cmd
+++ cmds/em/cnt.cmd
@@ -1,13 +1,19 @@
 # require mrfioc2,2.2.0-rc5
 # require iocStats,ae5d083
+# Sec-Sub:Dis-Dev-Idx
 
-epicsEnvSet("IOC", "DIACNT")
-epicsEnvSet("DEV1", "CNT0")
+epicsEnvSet("SEC", "ICS")
+epicsEnvSet("SUB", "LAB")
+epicsEnvSet("DIS", "DIA")
+epicsEnvSet("DEV", "CNT0")
+# epicsEnvSet("DEV1", "CNT0")
+epicsEnvSet("IOCNAME", "$(SEC)-$(SUB):$(DIS)-$(DEV)")
 
 # dbLoadRecords("evr-mtca-300u-ess.db","EVR=$(DEV1), SYS=$(IOC), D=$(DEV1), FEVT=$(ESSEvtClockRate)")
+# dbLoadRecords("./cnt.db", "SYS=$(IOC), D=$(DEV1), IOC=$(IOCNAME)")
 
-dbLoadRecords("./cnt.db", "SYS=$(IOC), D=$(DEV1)")
+dbLoadRecords("./cnt.db", "IOC=$(IOCNAME)")
 
 iocInit()
 
-dbl > "$(IOC)-$(DEV1)_PVs.list"
+dbl > "$(IOCNAME)_PVs.list"
