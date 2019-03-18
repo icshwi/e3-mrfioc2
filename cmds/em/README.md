5a110dbc2db3f34adc3994d8e869f7d39bffd8a3
Jerzy Jamroz <jerzy.jamroz@esss.se>
2019-03-18 15:49:35 +0100

    ESS naming test2

--- cmds/em/README.md
+++ cmds/em/README.md
@@ -1,1 +1,2 @@
 Engineering Manual tests on evr0 and evr1 ics lab test setup.
+
--- cmds/em/cnt.cmd
+++ cmds/em/cnt.cmd
@@ -1,19 +1,8 @@
 # require mrfioc2,2.2.0-rc5
 # require iocStats,ae5d083
-# Sec-Sub:Dis-Dev-Idx
 
-epicsEnvSet("SEC", "ICS")
-epicsEnvSet("SUB", "LAB")
-epicsEnvSet("DIS", "DIA")
-epicsEnvSet("DEV", "CNT0")
-# epicsEnvSet("DEV1", "CNT0")
-epicsEnvSet("IOCNAME", "$(SEC)-$(SUB):$(DIS)-$(DEV)")
-
-# dbLoadRecords("evr-mtca-300u-ess.db","EVR=$(DEV1), SYS=$(IOC), D=$(DEV1), FEVT=$(ESSEvtClockRate)")
-# dbLoadRecords("./cnt.db", "SYS=$(IOC), D=$(DEV1), IOC=$(IOCNAME)")
-
-dbLoadRecords("./cnt.db", "IOC=$(IOCNAME)")
-
+epicsEnvSet("IOCNAME0", "ICS-LAB:DIA-CNT-0")
+dbLoadRecords("./cnt.db", "IOC=$(IOCNAME0)")
 iocInit()
 
 dbl > "$(IOCNAME)_PVs.list"
