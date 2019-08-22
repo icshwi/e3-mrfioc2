2eca4948b8637d5177b7dbd353cfb5245624c8d7
Jerzy Jamroz <jerzy.jamroz@esss.se>
2019-08-22 16:05:44 +0200

    update.bash script correction in order to download correct files

--- iocsh/update.bash
+++ iocsh/update.bash
@@ -3,12 +3,17 @@
 # Author: Jerzy Jamroz
 #
 # Description:
-#   It downloads the latest .iocsh files for this project.
+#   It downloads the latest .iocsh files for this project. Tested withing gitlab.
 # Run Information:
 #   It has to be executed manually at the moment.
 
 # define the .iocsh weblink
 REPO=https://gitlab.esss.lu.se/icshwi/hwi-tim-prod/tree/master/iocsh
+echo "Update from the repository:"
+echo ${REPO}
+# https://gitlab.esss.lu.se/icshwi/hwi-tim-prod/blob/master/iocsh
+REPOFILE=`echo $REPO | sed -e 's/tree/raw/g'`
+echo ${REPOFILE}
 
 ## curl -s https://gitlab.esss.lu.se/icshwi/hwi-tim-prod/tree/master/iocsh | grep -i .iocsh | grep -i span | sed -e "s/<span>//g" | sed -e "s/<\/span>//g"
 ## arr_tmp=`curl -s https://gitlab.esss.lu.se/icshwi/hwi-tim-prod/tree/master/iocsh | grep -i .iocsh | grep -i span | sed -e "s/<span>//g" | sed -e "s/<\/span>//g"`
@@ -21,6 +26,7 @@ for el in "${arr[@]}"
 do
     if [[ $el != _* ]]; then
         echo "$el"
-        curl ${REPO}/${el} > ${el}
+        curl ${REPOFILE}/${el} > ${el}
     fi
 done
+
