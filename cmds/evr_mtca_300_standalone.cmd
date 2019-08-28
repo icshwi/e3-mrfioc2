82af6c1cc0af8ba97da292ed309760fcaeb3c600
Jerzy Jamroz <jerzy.jamroz@esss.se>
2019-08-28 12:43:39 +0200

    recover the master config

--- cmds/evr_mtca_300_standalone.cmd
+++ cmds/evr_mtca_300_standalone.cmd
@@ -4,13 +4,13 @@ epicsEnvSet("ENGINEER","Han")
 epicsEnvSet("LOCATION","Table at ICS Tuna Lab")
 epicsEnvSet("EPICS_CA_MAX_ARRAY_BYTES","10000000")
 
-epicsEnvSet("IOC", "MTCA5U")
+epicsEnvSet("IOC", "MTCA")
 epicsEnvSet("DEV", "EVR")
 
 # Not use in this script, but it is needed for the expansion.
 epicsEnvSet("MainEvtCODE" "14")
 
-iocshLoad("$(mrfioc2_DIR)/evr-mtca-300.iocsh", "S=$(IOC), DEV=$(DEV), PCIID=09:00.0")
+iocshLoad("$(mrfioc2_DIR)/evr-mtca-300.iocsh", "S=$(IOC), DEV=$(DEV), PCIID=0b:00.0")
 #-iocshLoad("$(mrfioc2_DIR)/evr-mtca-300.iocsh", "S=$(IOC), DEV=$(DEV), PCIID=0b:00.0, RT=wtRT")
 
 iocInit()
@@ -19,5 +19,4 @@ dbl > "${IOC}_PVs.list"
 
 # The following script should be called after iocInit()
 iocshLoad("$(mrfioc2_DIR)/evr-standalone-mode.iocsh", "S=$(IOC), DEV=$(DEV)")
-
 
