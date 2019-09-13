e0145bba090296eda07343d4eb506183f16f7026
Jerzy Jamroz <jerzy.jamroz@esss.se>
2019-09-13 16:20:37 +0200

    EVG integrated into master.

---
+++ cmds/evg.cmd
@@ -0,0 +1,32 @@
+# .cmd
+# ====
+epicsEnvSet("TOP", "$(E3_CMD_TOP)/..")
+iocshLoad("$(TOP)/iocsh/env-init.iocsh")
+
+epicsEnvSet("LOCATION","MBL-070ROW:CNPW-U-017:AMC2")
+
+epicsEnvSet("IOC", "TM")
+epicsEnvSet("DEV", "EVG")
+
+# Select the software release
+require mrfioc2, 2.2.0-rc6
+
+# INIT (select your create)
+## 5U
+## iocshLoad("$(TOP)/iocsh/evg-mtca-init.iocsh", "IOC=$(IOC), DEV=$(DEV), PCIID=$(MTCA_5U_PCIID7)")
+## 9U
+## iocshLoad("$(TOP)/iocsh/evg-mtca-init.iocsh", "IOC=$(IOC), DEV=$(DEV), PCIID=$(MTCA_9U_PCIID2)")
+## 3U
+iocshLoad("$(TOP)/iocsh/evg-mtca-init.iocsh", "IOC=$(IOC), DEV=$(DEV), PCIID=$(MTCA_3U_PCIID6)")
+
+iocInit()
+
+# RUNTIME
+# =======
+iocshLoad("$(TOP)/iocsh/evg-mtca-run.iocsh", "IOC=$(IOC), DEV=$(DEV), INTREF=''")
+# Add normal operation sequencer0 for tests
+iocshLoad("$(TOP)/iocsh/_evg-seq0-run.iocsh", "IOC=$(IOC), DEV=$(DEV)")
+
+# dbl > "${IOC}_PVs.list"
+
+#EOF
