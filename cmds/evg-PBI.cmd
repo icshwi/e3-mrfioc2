76ee0ec3d358781149160bdd32fa9f063b3f22b8
Jerzy Jamroz <jerzy.jamroz@esss.se>
2019-10-30 15:28:03 +0100

    PBI event sequence delivery - devel.

---
+++ cmds/evg-PBI.cmd
@@ -0,0 +1,25 @@
+# .cmd
+# ====
+epicsEnvSet("TOP", "$(E3_CMD_TOP)/..")
+iocshLoad("$(TOP)/iocsh/env-init.iocsh")
+
+epicsEnvSet("LOCATION","PBI LAB")
+
+epicsEnvSet("IOC", "TM")
+epicsEnvSet("DEV", "EVG")
+
+# Select the software release
+require mrfioc2, 2.2.0-rc6
+
+iocshLoad("$(TOP)/iocsh/evg-mtca-init.iocsh", "IOC=$(IOC), DEV=$(DEV), PCIID=$(MTCA_3U_PCIID6)")
+
+iocInit()
+
+# RUNTIME
+# =======
+iocshLoad("$(TOP)/iocsh/evg-mtca-run.iocsh", "IOC=$(IOC), DEV=$(DEV), INTREF=''")
+iocshLoad("$(TOP)/iocsh/evg-seq0-NBP-run.iocsh", "IOC=$(IOC), DEV=$(DEV)")
+
+# dbl > "${IOC}_PVs.list"
+
+#EOF
